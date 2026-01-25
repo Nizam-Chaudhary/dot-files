7a0421fa75580638424c3ef5304908acfcca73f5
Nizam Chaudhary <ChaudharyNizam10@gmail.com>
2026-01-25 10:09:46 +0000

    env for gpg

--- bash/.bash/envs.bash
+++ bash/.bash/envs.bash
@@ -15,3 +15,6 @@ export FZF_DEFAULT_OPTS=" \
 --color=marker:#B4BEFE,fg+:#CDD6F4,prompt:#CBA6F7,hl+:#F38BA8 \
 --color=selected-bg:#45475A \
 --color=border:#6C7086,label:#CDD6F4"
+
+# GPG TTY
+export GPG_TTY=$(tty)
