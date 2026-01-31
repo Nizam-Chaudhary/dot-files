d5b5eb3f205c087c8526db5dc4b6a40b2663aca7
Nizam Chaudhary <ChaudharyNizam10@gmail.com>
2026-01-31 23:59:11 +0530

    feat: git ouath update

--- zsh/.zsh/zinit.zsh
+++ zsh/.zsh/zinit.zsh
@@ -166,3 +166,13 @@ zinit light g-plane/pnpm-shell-completion
 # Zsh VI Mode (Shallow clone)
 # zinit ice wait lucid depth"1"
 # zinit light jeffreytse/zsh-vi-mode
+
+# Load a few important annexes, without Turbo
+# (this is currently required for annexes)
+zinit light-mode for \
+    zdharma-continuum/zinit-annex-as-monitor \
+    zdharma-continuum/zinit-annex-bin-gem-node \
+    zdharma-continuum/zinit-annex-patch-dl \
+    zdharma-continuum/zinit-annex-rust
+
+### End of Zinit's installer chunk
